4bf60ebcabe8dfb90fa16572b883be46cce0376c
Malcolm Abdullah <mabdullah4562@gmail.com>
2024-06-10 00:52:14 -0500

    final update

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -12,29 +12,41 @@
 
 function createCharacter(name, gender, characterClass) {
   // TODO: Implement this function
-  return{
-    getName:function(){
-        return name;
+  // Use closures to encapsulate the character's data
+  return {
+    getName: function() {
+      return name;
     },
-    getGender: function(){
+    getGender: function() {
       return gender;
     },
-    getClass: function(){
+    getClass: function() {
       return characterClass;
     }
-  }
+  };
 }
 
-document.getElementById("generateHero").addEventListener("click", function(e) {
+document.getElementById("characterForm").addEventListener("submit", function(e) {
   e.preventDefault();
 
   // TODO: Get form values
-  let heroName = document.getElementById("heroName").value;
-  let heroGender = document.getElementById("heroGender").value;
-  let heroClass = document.getElementById("heroClass").value;
-  // TODO: Create character
-  let newCharacter = createCharacter(heroName, heroGender, heroClass);
-  displayCharacter(newCharacter);
+  const heroName = document.getElementById("heroName").value;
+  const heroGender = document.getElementById("heroGender").value;
+  const heroClass = document.getElementById("heroClass").value;
 
+  // TODO: Create character
+  let newCharacter = createCharacter(heroName, heroGender, heroClass)
+  
   // TODO: Display character information
+  displayCharacter(newCharacter);
 });
+
+function displayCharacter(character) {
+  let characterOutput = document.getElementById("characterOutput");
+  characterOutput.innerHTML = `
+    <h2>Character Information</h2>
+    <p><strong>Name:</strong> ${character.getName()}</p>
+    <p><strong>Gender:</strong> ${character.getGender()}</p>
+    <p><strong>Class:</strong> ${character.getClass()}</p>
+  `;
+}
