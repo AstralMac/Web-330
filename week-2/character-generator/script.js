7ca134c3544f2e148ec528958e254085e45ac7f3
Malcolm Abdullah <mabdullah4562@gmail.com>
2024-06-09 20:48:03 -0500

    Update

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -3,23 +3,38 @@
   Chapter 1
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: Malcolm Abdullah
+  Date: 06-09-2024
+  Filename:script.js
 */
 
 "use strict";
 
 function createCharacter(name, gender, characterClass) {
   // TODO: Implement this function
+  return{
+    getName:function(){
+        return name;
+    },
+    getGender: function(){
+      return gender;
+    },
+    getClass: function(){
+      return characterClass;
+    }
+  }
 }
 
 document.getElementById("generateHero").addEventListener("click", function(e) {
   e.preventDefault();
 
   // TODO: Get form values
-
+  let heroName = document.getElementById("heroName").value;
+  let heroGender = document.getElementById("heroGender").value;
+  let heroClass = document.getElementById("heroClass").value;
   // TODO: Create character
+  let newCharacter = createCharacter(heroName, heroGender, heroClass);
+  displayCharacter(newCharacter);
 
   // TODO: Display character information
 });
